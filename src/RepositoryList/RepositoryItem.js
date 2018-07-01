dc957903b4770b6dfc9926c816b6ecf3ebd22477
David Garner <dprgarner@gmail.com>
2018-07-01 17:41:10 +0100

    Add a little bit of flare.

--- src/RepositoryList/RepositoryItem.js
+++ src/RepositoryList/RepositoryItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Stars from './Stars';
 import Watchers from './Watchers';
 
--- src/RepositoryList/RepositoryList.js
+++ src/RepositoryList/RepositoryList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import FlipMove from 'react-flip-move';
 
 import RepositoryItem from './RepositoryItem';
 import './styles.css';
 
-const RepositoryList = ({ repositories }) =>
-  repositories.edges.map(({ node }) => (
-    <div key={node.id} className="RepositoryItem">
-      <RepositoryItem {...node} />
-    </div>
-  ));
+const RepositoryList = ({ repositories }) => (
+  <FlipMove verticalAlignment="bottom" maintainContainerHeight>
+    {repositories.edges.map(({ node }) => (
+      <div key={node.id} className="RepositoryItem">
+        <RepositoryItem {...node} />
+      </div>
+    ))}
+  </FlipMove>
+);
 
 export default RepositoryList;
